024cebdd6373d897b4e41a6f2a209e9b53030721
GuileVieira <guilherme.vieira16@hotmail.com>
2024-11-06 15:22:10 -0300

    base projeto criada

---
+++ src/main/java/estudos/web/api/WebApiSpringApplication.java
@@ -0,0 +1,13 @@
+package estudos.web.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebApiSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebApiSpringApplication.class, args);
+	}
+
+}
